e4391a30b676401253b81f1dc22cc45541895ed5
sujato <sujato@gmail.com>
2021-07-03 14:58:42 +1000

    specify base url

--- rocket.config.mjs
+++ rocket.config.mjs
@@ -1,7 +1,9 @@
 import { rocketLaunch } from '@rocket/launch';
 import { rocketSearch } from '@rocket/search';
 import { rocketBlog } from '@rocket/blog';
+import { absoluteBaseUrlNetlify } from '@rocket/core/helpers';
 
 export default {
   presets: [rocketLaunch(), rocketSearch(), rocketBlog()],
+  absoluteBaseUrl: absoluteBaseUrlNetlify('http://localhost:8080'),
 };
